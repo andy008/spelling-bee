b24cdd65011da267bbaeee232ac66ba84aada3a3
andy008 <andy@infomaxim.com>
2022-07-09 19:08:44 +1000

    fix: context type

--- src/helpers/context.tsx
+++ src/helpers/context.tsx
@@ -1,22 +1,6 @@
 import { createContext, useState } from 'react';
 
-type UserProviderResult = ReturnType<typeof UserProvider>;
-const UserContext = createContext<UserProviderResult>({
-    user: {
-        id: 1,
-        name: '',
-        score: 0,
-        drills: 0,
-        correct: 0,
-        incorrect: 0,
-        avatar: '',
-    },
-    setUser: () => {},
-});
-
-export default UserContext
-
-
+export const UserContext = createContext(null as any);
 
 export function UserProvider({children}){
     const [user, setUser] = useState({});
